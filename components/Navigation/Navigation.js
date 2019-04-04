aef2c2ec81129381f58e0d17f2fb74ae495ed9fc
Sussie Casasola <sus.casasola@gmail.com>
2019-04-03 23:24:51 -0600

    feat(Navigation): Use links properly

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,10 +1,17 @@
+import Link from 'next/link';
 import styles from './styles';
 
 const Navigation = ({ currentUrl }) => (
   <nav {...styles()}>
-    <a href='/' className={currentUrl === '/home' ? 'active' : ''}>Home</a>
-    <a href='/about' className={currentUrl === '/about' ? 'active' : ''}>About</a>
-    <a href='/blog' className={currentUrl === '/blog' ? 'active' : ''}>Blog</a>
+    <Link href='/'>
+      <a className={currentUrl === '/home' ? 'active' : ''}>Home</a>
+    </Link>
+    <Link href='/about'>
+      <a className={currentUrl === '/about' ? 'active' : ''}>About</a>
+    </Link>
+    <Link href='/blog'>
+      <a className={currentUrl === '/blog' ? 'active' : ''}>Blog</a>
+    </Link>
   </nav>
 );
 
