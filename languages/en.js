f55960b442a4204e5d4ad83fc7613505b28dc95e
Sussie Casasola <sus.casasola@gmail.com>
2020-04-26 18:21:02 -0500

    fix(Footer): Setup correctly the year and remove flags

--- languages/en.js
+++ languages/en.js
@@ -1,17 +1,13 @@
 const en = {
-  meta_description_home:
-    "Welcome to my website :) This site was created to practice and write about what I'm learning.",
-  meta_description_about: 'This part of the site describes a little more about who I am.',
+  meta_description_home: 'Welcome to my website :)',
   meta_description_blog: "This is the list of articles I've recently published",
-  navigation_home: 'Home',
-  navigation_about: 'About',
-  navigation_blog: 'Blog',
+  navigation_home: '🏠 Home',
+  navigation_blog: '📝 Blog',
   home_content_field: 'Home',
-  about_content_field: 'About me',
   blog_page_title: 'My articles',
   article_published_on: 'Published on: ',
   article_read_more: 'Read more...',
-  footer_switch_lang: '🇲🇽 Switch to Spanish',
+  footer_switch_lang: '💬 Switch to Spanish',
   footer_built_with: 'Built with Next.js, Contentful and Netlify',
 };
 
