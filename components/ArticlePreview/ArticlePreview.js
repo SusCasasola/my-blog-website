a3d1f4d8104f46ff112fa8f02d568c6472853106
Sussie Casasola <sus.casasola@gmail.com>
2020-04-26 20:05:24 -0500

    refactor(ArticlePreview): Add the click on the whole preview

--- components/ArticlePreview/ArticlePreview.js
+++ components/ArticlePreview/ArticlePreview.js
@@ -6,20 +6,18 @@ import translate from 'utils/translate';
 import formatDate from 'utils/formatDate';
 
 const ArticlePreview = ({ title, description, publishDate, slug, currentLang }) => (
-  <article>
-    <h2>{title}</h2>
-    <span>{`${translate(currentLang, 'article_published_on')}${formatDate(publishDate)}`}</span>
-    <p>
-      {description}
-      <br />
-      <Link
-        as={`/${currentLang}/blog/${slug}`}
-        href={{ pathname: '/article', query: { slug, lang: currentLang } }}
-      >
-        <a>{translate(currentLang, 'article_read_more')}</a>
-      </Link>
-    </p>
-  </article>
+  <Link
+    as={`/${currentLang}/blog/${slug}`}
+    href={{ pathname: '/article', query: { slug, lang: currentLang } }}
+  >
+    <a>
+      <article>
+        <h2>{title}</h2>
+        <span>{`${translate(currentLang, 'article_published_on')}${formatDate(publishDate)}`}</span>
+        <p>{description}</p>
+      </article>
+    </a>
+  </Link>
 );
 
 ArticlePreview.propTypes = {
