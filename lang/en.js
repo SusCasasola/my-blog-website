ca2db6bc95b50b614f6f34e86939d51b1778d553
Sussie Casasola <sus.casasola@gmail.com>
2020-09-17 17:57:48 -0500

    feat(all): Reorder some folders

---
+++ lang/en.js
@@ -0,0 +1,16 @@
+const en = {
+  meta_description_blog: "This is the list of all articles I've published",
+
+  navigation_home: 'Home',
+  navigation_blog: 'Articles',
+
+  blog_page_title: 'All my articles',
+
+  article_published_on: 'Published on: ',
+  article_read_more: 'Read more...',
+
+  footer_switch_lang: '💬 Switch to Spanish',
+  footer_built_with: 'Built with Next, Contentful and Netlify',
+};
+
+export default en;
---
+++ lang/es.js
@@ -0,0 +1,16 @@
+const es = {
+  meta_description_blog: 'Esta es la lista de todos los artículos que he publicado.',
+
+  navigation_home: 'Inicio',
+  navigation_blog: 'Artículos',
+
+  blog_page_title: 'Todos mis artículos',
+
+  article_published_on: 'Publicado el: ',
+  article_read_more: 'Leer más...',
+
+  footer_switch_lang: '💬 Cambiar a Inglés',
+  footer_built_with: 'Construido con Next, Contentful y Netlify',
+};
+
+export default es;
